732c5ea22720a1456137aa2c297e435d33d18ef3
contactmatthew <contact.james.matthew@gmail.com>
2026-01-03 21:25:41 +0800

    Fix config.js import order

--- client/src/config.js
+++ client/src/config.js
@@ -2,10 +2,11 @@
 // In production, set REACT_APP_API_URL environment variable
 // For local development, it defaults to localhost:5000
 
+import axios from 'axios';
+
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 // Configure axios base URL
-import axios from 'axios';
 axios.defaults.baseURL = API_URL;
 
 export default API_URL;
